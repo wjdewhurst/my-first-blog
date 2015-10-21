f18cd12aedf7a967e9abe81b53ca9d8d5c3430bb
Warren <wjdewhurst@googlemail.com>
2015-10-21 12:10:08 +0100

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+	url(r'^$', views.post_list, name='post_list'),
+	# (?<pk>[0-9]+) = Everything here will be transferred to a variable called 'pk'.
+	# [0-9] = Must be a number.
+	# + = Must be one or more digits.
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 # Dot before 'models' means current directory/application
 from .models import Post
@@ -6,5 +6,10 @@ from .models import Post
 
 # Function called 'post_list', takes request, returns function 'render' to render template.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
-    return render(request, 'blog/post_list.html', {"posts": posts})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+	return render(request, 'blog/post_list.html', {"posts": posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
+	
